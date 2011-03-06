a534580c49998428dab9bf39f560778e009e727c
Kasper Langer <kasperlanger@me.com>
2011-03-06 10:25:24 +0100

    Comitting first version of mapped-reference.core

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mapped-reference "1.0.0-SNAPSHOT"
+(defproject mapped-reference "0.1.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                    [org.clojure/clojure-contrib "1.2.0"]])
--- src/mapped_reference/core.clj
+++ src/mapped_reference/core.clj
@@ -1,1 +1,30 @@
 (ns mapped-reference.core)
+
+(defprotocol MappedAtomProtocol
+  (mapped-swap! [this fn]))
+
+(extend clojure.lang.Atom
+  MappedAtomProtocol
+  {:mapped-swap! clojure.core/swap!})
+    
+(deftype MappedAtom [atom-ref map-fn update-fn]
+  MappedAtomProtocol
+  (mapped-swap! [this fun]
+		(map-fn (mapped-swap!
+				atom-ref 
+				(fn [old] (update-fn old (fun (map-fn old)))))))
+
+  clojure.lang.IRef
+  (deref [this] (map-fn @atom-ref))
+  (removeWatch [this key] (remove-watch atom-ref [this key]))
+  (addWatch [this key callback]
+	    (add-watch atom-ref
+		       [this key] 
+		       (fn [key ref old-val new-val]
+			 (let [mapped-old (map-fn old-val)
+			       mapped-new (map-fn new-val)]
+			   (when (not= mapped-old mapped-new)
+			     (callback key ref mapped-old mapped-new)))))))
+  
+(defn mapped-atom [atom-ref map-fn update-fn]
+  (MappedAtom. atom-ref map-fn update-fn))
