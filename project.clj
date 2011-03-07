a4c219c58824622cc5954ab9246547895a2b57ba
Kasper Langer <kasperlanger@me.com>
2011-03-07 07:19:00 +0100

    Adding swank-clojure to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject mapped-reference "0.1.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                   [org.clojure/clojure-contrib "1.2.0"]])
+                   [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]] )
